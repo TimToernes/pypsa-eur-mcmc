fixed issue with the calculation of theta in calc_sigme

scripts/calc_sigma.py
@@ -74,8 +74,10 @@ def worker(q,thetas,mcmc_variables,co2_budget,q_proc_done):
                 theta['c'] = network.chain
                 theta['a'] = network.accepted
                 #theta['val'] = get_theta(network,mcmc_variables,co2_budget)
-                country_emis = get_country_emis(network)
-                theta['val'] = np.array([country_emis[v] for v in mcmc_variables])/co2_budget
+                #country_emis = get_country_emis(network)
+                #theta['val'] = np.array([country_emis[v] for v in mcmc_variables])/co2_budget
+                theta['val'] = str_to_theta(network.theta)
+
                 thetas.put(theta)
                 del(network)
 

scripts/mh_chain.py
@@ -262,7 +262,7 @@ if __name__=='__main__':
     
         alpha = np.random.rand()
         # Get the likelihood estimate pr_i from the network 
-        print(str_to_theta(network.theta))
+        #print(str_to_theta(network.theta))
         network, pr_i, theta_proposed, theta_old = sample(network)
         # Accept or reject the sample 
         if alpha<pr_i: # Sample accepted, save solved network