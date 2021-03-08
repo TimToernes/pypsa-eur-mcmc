bac48479e10ab8beed64ef73f20cb9a4280a122c
Tim's Macbook <ttp@mpe.au.dk>
2021-03-08 10:09:02 +0100

    fixed co2 issue

--- scripts/plotting2.py
+++ scripts/plotting2.py
@@ -1,5 +1,5 @@
 #%%
-from scripts.initialize_network import set_link_locations
+#from scripts.initialize_network import set_link_locations
 import pypsa
 import os 
 import csv
@@ -16,8 +16,8 @@ import time
 import scipy
 import matplotlib.pyplot as plt
 import seaborn as sns
-from _mcmc_helpers import calc_co2_emis_pr_node
-os.chdir('..')
+from _mcmc_helpers import *
+#os.chdir('..')
 
 
 override_component_attrs = pypsa.descriptors.Dict({k : v.copy() for k,v in pypsa.components.component_attrs.items()})
@@ -37,6 +37,8 @@ override_component_attrs["Link"].loc["p4"] = ["series","MW",0.,"4th bus output",
 run_name = 'h100_model'
 #run_name = 'h99_model'
 network = pypsa.Network(f'results/{run_name}/network_c0_s1.nc',override_component_attrs=override_component_attrs)
+mcmc_variables = read_csv(network.mcmc_variables)
+mcmc_variables = [row[0]+row[1] for row in mcmc_variables]
 df_secondary = pd.read_csv(f'results/{run_name}/result_secondary_metrics.csv',index_col=0)
 df_sum = pd.read_csv(f'results/{run_name}/result_sum_vars.csv',index_col=0)
 df_gen_p = pd.read_csv(f'results/{run_name}/result_gen_p.csv',index_col=0)
@@ -44,19 +46,22 @@ df_co2 = pd.read_csv(f'results/{run_name}/result_co2_pr_node.csv',index_col=0)
 df_chain = pd.read_csv(f'results/{run_name}/result_chain.csv',index_col=0)
 df_links = pd.read_csv(f'results/{run_name}/result_links.csv',index_col=0)
 df_co2 = pd.read_csv(f'results/{run_name}/result_co2_pr_node.csv',index_col=0)
+df_store = pd.read_csv(f'results/{run_name}/result_store_p.csv',index_col=0)
 
 theta = pd.read_csv(f'results/{run_name}/theta.csv',index_col=0)
-theta.columns = [f'theta_{x}' for x in range(34)]+['s','c','a']
+theta.columns = [x for x in mcmc_variables+['s','c','a']]
 theta['id'] = theta.index
 
+
+emis_1990 = 1481895952.8
 
 #%%#########################################
 ####### Data postprocessing ################
 ############################################
 
-
 def set_link_locataions(network):
     network.links['location'] = ""
+    network.generators['location'] = ""
 
     query_string = lambda x : f'bus0 == "{x}" | bus1 == "{x}" | bus2 == "{x}" | bus3 == "{x}" | bus4 == "{x}"'
     id_co2_links = network.links.query(query_string('co2 atmosphere')).index
@@ -77,6 +82,9 @@ def set_link_locataions(network):
             idx = network.links.query(query_string(bus))['location'].index
             network.links.loc[idx,'location'] = country
 
+            idx = network.generators.query(f"bus == '{bus}'")['location'].index
+            network.generators.loc[idx,'location'] = country
+
     # Links connecting to co2 atmosphere without known location are set to belong to EU
     idx_homeless = network.links.query(query_string('co2 atmosphere')).query('location == ""').index
     network.links.loc[idx_homeless,'location'] = 'EU'
@@ -86,71 +94,84 @@ def set_link_locataions(network):
 
 cost_increase = (df_secondary.system_cost-network.objective_optimum)/network.objective_optimum*100
 
-base_emis = 38750000
-co2_red = (base_emis - df_secondary.loc[:,'co2_emission'])/base_emis*100
+base_emis = emis_1990
 
 df_secondary['cost_increase'] = cost_increase
-df_secondary['co2_reduction'] = co2_red
+df_secondary['co2_reduction'] = 100 - df_co2.sum(axis=1)/base_emis * 100
 
+
+# Dataset with with aggregated technology capacities 
+network = set_link_locataions(network)
+idx = network.links.query('location != "EU" & location != ""').index
+df_link_sum = df_links[idx].groupby(network.links.carrier,axis=1).sum()
+
+df_gen_sum = df_gen_p.groupby(network.generators.carrier,axis=1).sum()
+#df_gen_sum.pop('oil')
+
+df_tech_sum = pd.concat([df_link_sum,df_gen_sum],axis=1)
+df_tech_sum['wind'] = df_tech_sum[['offwind','offwind-ac','offwind-dc','onwind']].sum(axis=1)
+
+
+###########################################
+############### filters ###################
+###########################################
 # create filter for 150 CO2 reduction compared to coal production 
 
 country_loads = network.loads_t.p.sum().groupby(network.buses.country).sum()
 co2_emis_pr_ton = 0.095
-alowable_emis_pr_country = country_loads * co2_emis_pr_ton *1.5
+alowable_emis_pr_country = country_loads * co2_emis_pr_ton *1.5 * network.snapshot_weightings[0]
 df_co2_contry = df_co2.groupby(network.buses.country,axis=1).sum()
+df_co2_contry = df_co2_contry.loc[:,df_co2_contry.columns != '']
 filt_co2_150p = (df_co2_contry<alowable_emis_pr_country).all(axis=1)
 
 # create a filter for minimum backup capacity
 
 df_ocgt = df_gen_p.loc[:,network.generators.carrier=='OCGT']
-
 ocgt_pr_country = df_ocgt.groupby(network.generators.bus,axis=1).sum().groupby(network.buses.country,axis=1).sum()
-
 country_max_load = network.loads_t.p.max().groupby(network.buses.country).sum()
-
 filt_backup = (ocgt_pr_country>0.5*country_max_load).all(axis=1)
 
-# Dataset with generators from links 
-network = set_link_locations(network)
-idx = network.links.query('location != "EU" & location != ""').index
-df_link_sum = df_links[idx].groupby(network.links.carrier,axis=1).sum()
+# filter for 10% cost increase 
+filt_cost = df_secondary['cost_increase'] < 10
+
 
 #%%##########################################
 # ############ plots ########################
 # ###########################################
 # Corrolelogram cost vs co2 
 
-df = df_secondary[['system_cost']]
+df = df_secondary[['cost_increase']]
 df['co2_reduction'] = 100 - df_co2.sum(axis=1)/base_emis * 100
 
-sns_plot = sns.pairplot(df, kind="hist", diag_kind='kde')#,hue='co2<150p')
+df['filt_cost'] = filt_cost
+
+sns_plot = sns.pairplot(df, kind="hist", diag_kind='hist',hue='filt_cost')
 #plt.suptitle('Scenarios with less than 150% local emisons compared to 100% coal production')
 #plt.suptitle('Scenarios where all countries have more than 10% fosil fuel backup')
 
-#sns_plot.savefig(f'graphics/tech_{run_name}_150p_co2_cap.jpeg')
+sns_plot.savefig(f'graphics/cost_co2_{run_name}.jpeg')
 fig = sns_plot.fig
 fig.show()
 
 #%% Corrolelogram tech
 
 #df = df_sum[['OCGT', 'offwind-ac', 'offwind-dc', 'onwind', 'solar', 'transmission', 'H2', 'battery',]]
-df = df_link_sum[['OCGT','H2 Electrolysis','SMR']]
+df = df_tech_sum[['OCGT','H2 Electrolysis','SMR','solar','wind','gas']]
 #df = df[df_chain.s>200]
 #df['co2<150p'] = filt_co2_150p
 #df['10p_backup'] = filt_backup
 #df['c'] = df_chain.c
 #df = df[df_secondary.co2_reduction>50]
 #df['c'] = theta.c
-# with regression
-#sns.pairplot(df, kind="reg")
-#plt.show()
+df['filt_cost'] = filt_cost
+
  
 # without regression
-sns_plot = sns.pairplot(df, kind="hist", diag_kind='kde')#,hue='co2<150p')
-plt.suptitle('Scenarios with less than 150% local emisons compared to 100% coal production')
+sns_plot = sns.pairplot(df, kind="hist", diag_kind='hist',hue='filt_cost')
+#plt.suptitle('Scenarios with less than 150% local emisons compared to 100% coal production')
 #plt.suptitle('Scenarios where all countries have more than 10% fosil fuel backup')
 
-#sns_plot.savefig(f'graphics/tech_{run_name}_150p_co2_cap.jpeg')
+#sns_plot.savefig(f'graphics/tech_{run_name}.jpeg')
 fig = sns_plot.fig
 fig.show()
 
@@ -159,21 +180,22 @@ fig.show()
 
 df_secondary['transmission'] = df_sum['transmission']
 #df = sns.load_dataset('iris')
-df = df_secondary[['cost_increase','co2_reduction','autoarky','gini_co2','gini','transmission']]
+df = df_secondary[['cost_increase','gini_co2','gini',]]
+df['co2_reduction'] = df['co2_reduction'] = 100 - df_co2.sum(axis=1)/base_emis * 100
 #df = df[df_secondary.cost_increase<7]
 #df = df[df_secondary.co2_reduction>50]
 #df['co2<150p'] = filt_co2_150p
-df['10p_backup'] = filt_backup
+#df['10p_backup'] = filt_backup
 #df['c'] = theta.c
-# with regression
-#sns.pairplot(df, kind="reg")
-#plt.show()
-# without regression
-sns_plot = sns.pairplot(df, kind="hist", diag_kind='kde',hue='10p_backup')
-#plt.suptitle('Scenarios with less than 150% emisons compared to 100% coal production')
-plt.suptitle('Scenarios where all countries have more than 10% fosil fuel backup')
+df['filt_cost'] = filt_cost
 
-#sns_plot.savefig(f'graphics/secondary_{run_name}_10p_backup.jpeg')
+
+
+sns_plot = sns.pairplot(df, kind="hist", diag_kind='hist',hue='filt_cost')
+#plt.suptitle('Scenarios with less than 150% emisons compared to 100% coal production')
+#plt.suptitle('Scenarios where all countries have more than 10% fosil fuel backup')
+
+#sns_plot.savefig(f'graphics/secondary_{run_name}.jpeg')
 sns_plot.fig.show()
 
 #%% Plot of chain development over time 
@@ -198,30 +220,33 @@ sns.relplot(
     kind="line",
     height=5, aspect=.75,)
 
-#%%
+#%% plot of co2 emis 
 
-df_co2['DK0 1'] = df_co2[['DK0 0','DK3 0']].sum(axis=1)
-df_co2['GB0 0'] = df_co2[['GB4 0','GB5 0']].sum(axis=1)
+df = df_co2[['DE','DK','FR','ES','PL']]
 
-df_co2['cost_increase'] = df_secondary['cost_increase']
+df['filt_cost'] = filt_cost
 
-df = df_co2[['DK0 1','DE0 0','FR0 0','GB0 0','cost_increase']]
-#df = df[df_secondary.cost_increase<25]
-#df = df[df_secondary.co2_reduction>80]
-df['10p_backup'] = filt_backup
-#df['co2<150p'] = filt_co2_150p
 
-#df['c'] = theta.c
-# with regression
-#sns.pairplot(df, kind="reg")
-#plt.show()
- 
-# without regression
-sns_plot = sns.pairplot(df, kind="hist", diag_kind='kde',hue='10p_backup')
+sns_plot = sns.pairplot(df, kind="hist", diag_kind='hist',hue='filt_cost')
+plt.suptitle('CO2 emissions pr country [T CO2]')
+
 #sns_plot.map_lower(sns.regplot)
 #sns_plot.savefig('test2.pdf')
 
-sns_plot.savefig(f'graphics/theta_{run_name}_10p_backup.jpeg')
+sns_plot.savefig(f'graphics/co2_emissions_{run_name}.jpeg')
+sns_plot.fig.show()
+
+#%% plot of thetas 
+
+df = theta[['DK','DE','FR','PL']]
+
+sns_plot = sns.pairplot(df, kind="hist", diag_kind='hist')#,hue='10p_backup')
+plt.suptitle('CO2 emissions pr country [T CO2]')
+
+#sns_plot.map_lower(sns.regplot)
+#sns_plot.savefig('test2.pdf')
+
+sns_plot.savefig(f'graphics/co2_emissions_{run_name}.jpeg')
 sns_plot.fig.show()
 
 
